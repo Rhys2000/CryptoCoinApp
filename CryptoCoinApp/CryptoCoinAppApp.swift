02d3fbcf2f55253c4716488afc52911243aadf07
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-12 16:26:34 -0700

    Initial Commit

---
+++ CryptoCoinApp/CryptoCoinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinAppApp.swift
+//  CryptoCoinApp
+//
+//  Created by Rhys Julian-Jones on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
