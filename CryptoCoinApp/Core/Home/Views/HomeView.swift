533146450806eb345aa4f02851e0d96ad837e7f6
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-14 08:41:38 -0700

    Added a state variable to HomeView that based on the state changes the homeHeader button icons, starts an animation, and changes from Live Prices to Portfolio

--- CryptoCoinApp/Core/Home/Views/HomeView.swift
+++ CryptoCoinApp/Core/Home/Views/HomeView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
     var body: some View {
         ZStack {
             // Background layer
@@ -16,7 +19,7 @@ struct HomeView: View {
             
             //Content layer
             VStack {
-                Text("Header")
+                homeHeader
                 Spacer(minLength: 0)
             }
         }
@@ -29,5 +32,30 @@ struct HomeView_Previews: PreviewProvider {
             HomeView()
                 .navigationBarHidden(true)
         }
+    }
+}
+
+extension HomeView {
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none, value: showPortfolio)
+                .background(CircleCuttonAnimationView(animate: $showPortfolio))
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none, value: showPortfolio)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 90 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
     }
 }
