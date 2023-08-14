5e317023b3f3348f1e044805e71e994fffe05e1e
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-14 08:38:15 -0700

    Added iconName variables to make CircleButton component reusable and have the icon be taken in as an argument. Added some shadow to the back of the button

--- CryptoCoinApp/Core/Components/CircleButtonView.swift
+++ CryptoCoinApp/Core/Components/CircleButtonView.swift
@@ -8,17 +8,26 @@
 import SwiftUI
 
 struct CircleButtonView: View {
+    
+    let iconName: String
+    
     var body: some View {
-        Image(systemName: "heart.fill")
+        Image(systemName: iconName)
             .font(.headline)
             .foregroundColor(Color.theme.accent)
             .frame(width: 50, height: 50)
-            .background(Circle())
+            .background(
+                Circle()
+                    .foregroundColor(Color.theme.background)
+            )
+            .shadow(color: Color.theme.accent.opacity(0.25), radius: 10)
+            .padding()
     }
 }
 
 struct CircleButtonView_Previews: PreviewProvider {
     static var previews: some View {
-        CircleButtonView()
+        CircleButtonView(iconName: "heart.fill")
+            .previewLayout(.sizeThatFits)
     }
 }
