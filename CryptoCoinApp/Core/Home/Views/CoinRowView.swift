ddf6bbcbb5d7406fc6335a8042d9509cb1cf3841
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-14 15:20:27 -0700

    Created CoinRowView which will hold information about each crypto currency in the list displayed on the Home View under live prices

---
+++ CryptoCoinApp/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,73 @@
+//
+//  CoinRowView.swift
+//  CryptoCoinApp
+//
+//  Created by Rhys Julian-Jones on 8/14/23.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    let coin: Coin
+    let showHoldingsColumn: Bool
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            leftColumn
+            Spacer()
+            if(showHoldingsColumn) {
+                centerColumn
+            }
+            
+            rightColumn
+        }
+        .font(.subheadline)
+        .padding(.trailing)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+            .previewLayout(.sizeThatFits)
+    }
+}
+
+extension CoinRowView {
+    
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundColor(Color.theme.accent)
+        }
+    }
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.currencyAsAString())
+                .bold()
+            Text((coin.currentHoldings ?? 0).numberAsAString())
+        }
+        .foregroundColor(Color.theme.accent)
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.currencyAsAString())
+                .bold()
+                .foregroundColor(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.percentageAsAString() ?? "")
+                .foregroundColor((coin.priceChangePercentage24H ?? 0) >= 0 ? Color.theme.green : Color.theme.red)
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+    }
+}
